fa027cb3d4b3938737187db87ff3163987e6988e
rialgou <rialgou@gmail.com>
2023-08-29 02:51:03 -0400

    primer test con fallido para funcionalidad 1 de ohce

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+funcionamiento de ohce
+1. al iniciar ohce te saludara en base a intervalos de tiempo 
+Between 20 and 6 hours, ohce will greet you saying: ¡Buenas noches < your name >!
+Between 6 and 12 hours, ohce will greet you saying: ¡Buenos días < your name >!
+Between 12 and 20 hours, ohce will greet you saying: ¡Buenas tardes < your name >!
+
+2. cuando se introduce un palindromo ohce lo escribira en reversa y añadira ¡Bonita palabra!
+
+3. con Stop! dira Adios < your name>
+
+y cualquier palabra que ingreses sera un palindromo
+
+'''
+
+def ohce(name):
+    return ""
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from tarea1.main import ohce
+
+def test_ohce():
+    assert ohce("juan") == "¡Buenas tardes juan!"
+
+
